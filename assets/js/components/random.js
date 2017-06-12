da047cbd02ba77c7cfce498ba3f99c0ab008f765
Fiorellacr24 <fiorella.0624@gmail.com>
2017-06-11 21:40:22 -0500

    add styles

--- assets/js/components/random.js
+++ assets/js/components/random.js
@@ -1,18 +1,21 @@
 'use strict';
 
 const Coder = (update) =>{
-	const parent = $("<div class='container'></div>")
-	const containerCoder = $("<div class='cont-coder'></div>");
-	const imgCoder = $("<div class='img-coder'></div>");
-	const label = $("<label>Ingresa su nombre:</label>");
-	const nombre = $("<input type='text' placeholder='Nombre'>");
-	const comprobar = $("<button>Comprobar</button>")
+	const parent = $("<div class='container view-play f-roboto'></div>")
+	const containerCoder = $("<div class='cont-coder col-xs-12 col-sm-12 col-md-4 col-lg-4'></div>");
+	const imgCoder = $("<img class='img-responsive img-coder'></img>");
+	const containerInput = $("<div class='cont-input col-xs-12 col-sm-12 col-md-6 col-lg-6'></div>");
+	const label = $("<label class='white f-rokkitt'>Ingresa su nombre:</label><br>");
+	const nombre = $("<input class='form-control' type='text' placeholder='Nombre' id='focusedInput'>");
+	const comprobar = $("<button type='button' class='btn btn-warning'>COMPROBAR</button>")
 	
 	containerCoder.append(imgCoder);
-	containerCoder.append(label);
-	containerCoder.append(nombre);
-	containerCoder.append(comprobar);
+	containerCoder.append(containerInput);
+	containerInput.append(label);
+	containerInput.append(nombre);
+	containerInput.append(comprobar);
 	parent.append(containerCoder);
+	parent.append(containerInput);
 
 	return parent;
 }
