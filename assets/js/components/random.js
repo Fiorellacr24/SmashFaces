272648b332a41e34a95d5005941c2cb6cd520842
Fiorellacr24 <fiorella.0624@gmail.com>
2017-06-12 14:04:32 -0500

    update random

--- assets/js/components/random.js
+++ assets/js/components/random.js
@@ -1,14 +1,13 @@
 'use strict';
 
 const Coder = (coder, update) =>{
-    const parent = $("<div class='container view-play f-roboto'></div>")
-    const containerCoder = $("<div class='cont-coder col-xs-12 col-sm-12 col-md-4 col-lg-4'></div>");
+    const parent = $('<div class="container view-play f-roboto"></div>');
+    const containerCoder = $('<div class="cont-coder col-xs-12 col-sm-12 col-md-4 col-lg-4"></div>');
     const imgCoder = $('<img class="img-responsive img-coder"/>');
-    //<img src="assets/img/mexico/Alma%20Patricia%20Jimenez.JPG" alt="">
-    const containerInput = $("<div class='cont-input col-xs-12 col-sm-12 col-md-6 col-lg-6'></div>");
-    const label = $("<label class='white f-rokkitt'>Ingresa su nombre:</label><br>");
-    const nombre = $("<input class='form-control' type='text' placeholder='Nombre' id='focusedInput'>");
-    const comprobar = $("<button type='button' class='btn btn-warning comprobar'>COMPROBAR</button>");
+    const containerInput = $('<div class="cont-input col-xs-12 col-sm-12 col-md-6 col-lg-6"></div>');
+    const label = $('<label class="white f-rokkitt">Ingresa su nombre:</label><br>');
+    const nombre = $('<input class="form-control" type="text" placeholder="Nombre" id="focusedInput">');
+    const comprobar = $('<button type="button" class="btn btn-warning comprobar">COMPROBAR</button>');
 
     parent.hide();
     containerCoder.append(imgCoder);
@@ -23,45 +22,7 @@ const Coder = (coder, update) =>{
     $('.select').on('change', _ =>{
         parent.show();
 
-        /*select = $(this).val();
-        if(select === Lima){
-            state.peru = peru;
-            mostrarCoderPeru(coder, imgCoder, update);
-        }else{
-            state.mexico = mexico;
-            mostrarCoderMex(coder, imgCoder, update);
-        }*/
     });
     return parent;
 }
 
-/*const mostrarCoderPeru = (coder, imgCoder, update) =>{
-    imgCoder.attr("src", coder[Math.floor(Math.random()* (state.peru).length)].image);
-
-    return imgCoder;
-}
-
-const mostrarCoderMex = (coder, imgCoder, update) =>{
-    imgCoder.attr("src", coder[Math.floor(Math.random()* (state.mexico).length)].image);
-
-    return imgCoder;
-}
-
-function filter() {
-    let selectName = "";
-    selectName = (sede == 0) ? peru[i].sede : mexico[i].sede;
-    return filter;
-}
-
-function comprobar() {
-    if (getInputValue() === selectName()) {
-        alert("Correcto");
-        setTimeout(mostrarCoder(), 3000);
-    } else {
-        alert("Sigue intentando");
-    }
-};
-
-$(document).ready(function() {
-    $(".comprobar").click(comprobar);
-});*/
